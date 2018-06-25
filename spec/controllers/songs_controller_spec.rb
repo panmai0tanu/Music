c324f458847f1183128e6ba44941059c9270c41b
panmai0tanu <panmai_tanu@cmu.ac.th>
2018-06-25 12:23:22 +0700

    HW test rspec

--- spec/controllers/songs_controller_spec.rb
+++ spec/controllers/songs_controller_spec.rb
@@ -1,11 +1,11 @@
 require 'rails_helper'
 
 describe "songs controller spec", :type => :request do
-  it 'check 3 songs from controller' do
-    get '/song/html'
+  it 'check @songs variable has length = 3' do
+    get '/song/json'
     expect(assigns[:songlist].length).to eq(3)
   end
-  it 'check @songs variable' do
+  it 'check @songs variable instance of class Song' do
     get '/song/html'
     expect(assigns[:songlist].instance_of?(Songs.all.class))
   end
