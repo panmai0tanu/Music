6317d70ff300bd97188b8a07a4722f64fa793090
panmai0tanu <panmai_tanu@cmu.ac.th>
2018-06-25 10:06:12 +0700

    chang band

--- db/schema.rb
+++ db/schema.rb
@@ -12,25 +12,15 @@
 
 ActiveRecord::Schema.define(version: 2018_06_21_053054) do
 
-  create_table "beta", force: :cascade do |t|
-    t.text "name"
-    t.text "phone"
-    t.datetime "age"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
 
   create_table "songs", force: :cascade do |t|
     t.string "name"
-    t.string "brand"
+    t.string "band"
     t.string "album"
     t.string "genre"
     t.string "chord"
   end
 
-  create_table "users", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
+
 
 end
--- spec/controllers/songs_controller_spec.rb
+++ spec/controllers/songs_controller_spec.rb
@@ -3,10 +3,10 @@ require 'rails_helper'
 describe "songs controller spec", :type => :request do
   it 'check 3 songs from controller' do
     get '/song/html'
-    expect(assigns[:songs].length).to eq(3)
+    expect(assigns[:songlist].length).to eq(3)
   end
   it 'check @songs variable' do
     get '/song/html'
-    expect(assigns[:songs].instance_of?(Songs.all.class))
+    expect(assigns[:songlist].instance_of?(Songs.all.class))
   end
 end
