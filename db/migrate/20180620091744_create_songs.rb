d0a8523134418e8c667b0a9b61ed73af989a4f21
panmai0tanu <panmai_tanu@cmu.ac.th>
2018-06-25 10:22:14 +0700

    chang band last

--- db/migrate/20180620091744_create_songs.rb
+++ db/migrate/20180620091744_create_songs.rb
@@ -2,7 +2,7 @@ class CreateSongs < ActiveRecord::Migration[5.2]
   def change
     create_table :songs do |t|
       t.string :name
-      t.string :brand
+      t.string :band
       t.string :album
       t.string :genre
       t.string :chord
